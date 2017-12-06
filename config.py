70dd59d626ff0c085b9318ff7c75dc706849ae83
Te12944265-AMAHA <31605482+Te12944265-AMAHA@users.noreply.github.com>
2017-12-06 18:46:00 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+#motor
+frontLeftChannel    = 1 
+rearLeftChannel     = 2
+frontRightChannel   = 3
+rearRightChannel    = 4
+Expiration          = 0.1
+JoystickNum         = 1 #手柄的接口
+GyroNum             = 0 #陀螺仪的接口
+#Solenoid Setting
+Solenoid1Num        = 0 
+Solenoid2Num        = 1
+Solenoid3Num        = 2
+SolenoidUselessNum	= 3
+#Climbing Setting
+ClimbEff            = 0.6 #爬升马达的效率
+ClimbMotor          = 5 #爬升马达的接口
+#auto setting
+Wait                = 2 #在气杆3推出时等待的时间
+Time                = 5 #移动65英尺所需的时间
